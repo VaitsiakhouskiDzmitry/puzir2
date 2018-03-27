8aa0abafc6f77b1ebbb0a83ce113465b055b124b
main <tonnykrummer@gmail.com>
2018-03-27 21:50:28 +0300

    mei9n

---
+++ src/run.java
@@ -0,0 +1,18 @@
+public class run {
+    public static void main(String[] args) {
+        Person Vasia = new Person("Vasia", 5);
+        Person Peter = new Person("Peter", 10);
+        Vasia.countDifference(Peter);
+
+    }
+
+    public static int countDifferentBetweenPersons(Person pl, Person ps) {
+        return pl.getApples() ps.getApples();
+    }
+}
+
+
+
+
+
+
