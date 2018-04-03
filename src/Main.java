611e23160522f3fe78c55dff2548b0bed2a20f23
main <tonnykrummer@gmail.com>
2018-04-03 21:43:39 +0300

    mei9n

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
-public class Main {
+import java.util.Arrays;   //сортировка пузырьком
 
-
-
-            public static void main(String[] args) {
-
-    Scanner scanner =new Scanner(System.in);
-    System.out.println("введите число");
-    int number=scanner.nextInt();
-    String binary = Integer.toBinaryString(number);
-    System.out.println("исло"+number+"в 2ойСС="+binary);
-    String hex=Integer.toHexString(number);
-    System.out.println("в 8ой" +hex);
-    String octal=Integer.toOctalString(number);
-    System.out.println("в 16ой"+octal);
-
-
+public class main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{6, 5, 4, 3, 2, 4, -9};
+        bubbleSort(arr);
     }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+                System.out.println(Arrays.toString(arr));
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println("Sorted array: " + Arrays.toString(arr));
+
+        }
+    }
+
 }
