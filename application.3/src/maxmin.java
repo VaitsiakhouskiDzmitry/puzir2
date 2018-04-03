86a15b2eb07147e3ad935e36cd92aab685d56976
main <tonnykrummer@gmail.com>
2018-04-03 21:16:31 +0300

    mei9n

---
+++ application.3/src/maxmin.java
@@ -0,0 +1,11 @@
+public class maxmin {
+    public static void main(String[] args) {
+        private static int getMaxFromArray(int[]array);
+        int indexOfMax=0;
+        for(int i = 1;i<array.length;i++){
+            if(array[i]>array[indexOfMax]){
+
+            }
+        }
+    }
+}
---
+++ application.3/src/puzir.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;   //сортировка пузырьком
+
+public class puzir {
+    public static void main(String[] args) {
+        int[] arr = new int[]{6, 5, 4, 3, 2, 4, -9};
+        bubbleSort(arr);
+        private static int getMaxFromArray(int[]array);
+        int indexOfMax=0;
+        for(int i = 1;i<array.length;i++){
+            if(array[i]>array[indexOfMax]){
+
+            }
+    }
+    return array[indexOfMax];
+            }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+                System.out.println(Arrays.toString(arr));
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println("Sorted array: " + Arrays.toString(arr));
+
+        }
+    }
+
+
+}
+
